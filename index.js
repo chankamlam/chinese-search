c4de8826ab474449736c98297e0f7eb2e26dab3b
Ken <KENCHAN@KENCHAN-MacBook-Air.local>
2016-01-19 14:29:41 +0800

    V1.0.1

--- index.js
+++ index.js
@@ -12,16 +12,70 @@ var option = {
     'keyArr': null
 }
 
+var checkInit = () => {
+    if (option.indexArr == null) {
+        throw new Error('set up the index before using the method of data')
+        return
+    };
+    if (option.keyArr == null) {
+        throw new Error('set up the key before using the method of data')
+        return
+    };
+    if (redisClient == null) {
+        redisClient = redis.createClient({
+            'host': option.host,
+            'port': option.port
+        });
+    };
+}
+var clearAllKeys = (callback) => {
+    var a = (cb) => {
+        redisClient.keys('*', (err, r) => {
+            if (err) {
+                throw new Error('err in clearAllKeys')
+                return
+            };
+            var n = {}
+            n.r = r
+            cb(null, n)
+        })
+    }
+    var b = (n, cb) => {
+        redisClient.del(n.r, (err1, r1) => {
+            if (err1) {
+
+            };
+            if (r1 <= 0) {
+                throw new Error('err in clearAllKeys')
+                return
+            };
+            cb(null, n)
+        })
+    }
+    async.waterfall([a, b], (err, r) => {
+        if (err) {
+            throw new Error('err in clearAllKeys')
+            return
+        };
+        callback()
+    })
+}
+
 function search(opt) {
     if (!(this instanceof search)) {
         return new search(opt);
     }
     if (opt && util.isObject(opt)) {
-        opt.host || option.host = opt.host
-        opt.port || option.port = opt.port
+        opt.host || (option.host = opt.host)
+        opt.port || (option.port = opt.port)
     };
 }
-
+search.prototype.option = function(opt) {
+    if (opt && util.isObject(opt)) {
+        opt.host || (option.host = opt.host)
+        opt.port || (option.port = opt.port)
+    };
+}
 search.prototype.index = function(arr) {
     // body...
     if (arr && util.isArray(arr)) {
@@ -40,36 +94,55 @@ search.prototype.key = function(arr) {
         throw new Error('err in keyArr')
     };
 };
+search.prototype.query = function(str, cb) {
+    // body...
+    if (str && util.isString(str)) {
+        checkInit.apply(this)
+        redisClient.smembers(str,(err,r)=>{
+            if (err) {
+                throw new Error('err in redisClient')
+                return
+            };
+            var res=[]
+            r.forEach((item)=>{
+                res.push(JSON.parse(item))
+            })
+            cb(null,res)
+        })
+    } else {
+        throw new Error('err in query string')
+    };
+};
 search.prototype.data = function(arr) {
     // body...
 
     if (arr && util.isArray(arr)) {
-    	if (option.indexArr==null) {
-    		throw new Error('set up the index before using the method of data')
-    	};
-    	if (option.keyArr=null) {
-    		throw new Error('set up the key before using the method of data')
-    	};
-        if (redisClient == null) {
-            redisClient = redis.createClient({
-                'host': option.host,
-                'port': option.port
-            });
-        };
-
+        checkInit.apply(this)
         var a = (cb) => {
+            clearAllKeys.call(this, () => {
+                var n = {}
+                cb(null, n)
+            })
+        }
+        var b = (n, cb) => {
             arr.forEach((item) => {
                 var indexArr = option.indexArr
-                var keyArr = option.keyArr
                 indexArr.forEach((index) => {
-                    var wordArr = jieba.cut(item[index])
+                    var itemOfIndex = item[index]
+                    if (!itemOfIndex) {
+                        throw new Error('can not find the key in the data')
+                        return
+                    };
+                    var wordArr = jieba.cut(itemOfIndex)
                     var obj = {}
+                    var keyArr = option.keyArr
                     keyArr.forEach((key) => {
-                        if (item[key]) {
-                            throw new Error('can not find the key in data')
+                        var itemOfKey = item[key]
+                        if (!itemOfKey) {
+                            throw new Error('can not find the key in the data')
                             return
                         };
-                        obj[key] = item[key]
+                        obj[key] = itemOfKey
                     })
                     var n = {}
                     n['wordArr'] = wordArr
@@ -78,35 +151,35 @@ search.prototype.data = function(arr) {
                 })
             })
         }
-        var b = (n, cb) => {
+        var c = (n, cb) => {
             var wordArr = n.wordArr
             var obj = n.obj
             if (redisClient == null) {
                 throw new Error('err in redisClient')
                 return
             };
-            wordArr.forEach((item) => {
-                redisClient.sadd(item, obj, (err, r) => {
+            wordArr.forEach((word) => {
+                redisClient.sadd(word, JSON.stringify(obj), (err, r) => {
                     if (err) {
                         throw new Error('err in redisClient')
                         return
                     };
-                    cb(null,{})
+                    cb(null, {})
                 })
             })
 
         }
-        async.waterfall([a, b], (err, r) => {
+        async.waterfall([a, b, c], (err, r) => {
             if (err) {
                 throw new Error('err in async')
                 return
             };
-            return this;
         })
+        return this
 
     } else {
         throw new Error('err in dataArr')
     };
 };
 
-module.exports=search
+module.exports = search
